96e1a4d478a79b9437b4ebd302aeeb2cd3cf16ba
Gabriel Dias <gabriel_dias10@hotmail.com>
2022-08-08 13:43:37 -0300

    alterações

--- view/inicio-html.php
+++ view/inicio-html.php
@@ -1,7 +1,7 @@
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Menu Principal</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
@@ -18,7 +18,7 @@
 <?php endif; ?>
 <div class="container">
     <div class="jumbotron">
-        <h1><?php echo $titulo ?></h1>
+        <h1 class="inicio-title"><?php echo $titulo ?></h1>
     </div>
 
     <style>
